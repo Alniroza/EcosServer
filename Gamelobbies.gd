270d510d719d904e2ad205bbd266fe6ed34c31ac
Gcarvaja123 <gabriel.carvajal.14@sansano.usm.cl>
2020-12-05 00:34:42 -0300

    arreglo leaderboard

--- Gamelobbies.gd
+++ Gamelobbies.gd
@@ -17,8 +17,20 @@ remote func delete_gamelobby(gamelobby):
 	if self.has_node(gamelobby):
 		get_node(gamelobby).queue_free()
 		print("Borré el game lobby : ", gamelobby)
-	
-
+		
+remote func update_leaderboard(name_gamemode, places, lobby_name):
+	print(places)
+	if self.has_node(lobby_name) :
+		if get_node(lobby_name).score_received != true : 
+			if name_gamemode == "survival":
+				var new_places = places
+				new_places.invert()
+				get_parent().update_leaderboard(name_gamemode, new_places)
+			if name_gamemode == "deathmatch":
+				get_parent().update_leaderboard(name_gamemode, places)
+			if name_gamemode == "teamdeathmatch":
+				get_parent().update_leaderboard(name_gamemode, places)
+			get_node(lobby_name).score_received = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
